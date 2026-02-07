1062ee9f0c57daab1b393a679f8e693f139cc06d
sebastien <snr.lauret@gmail.com>
2026-02-07 16:48:46 +0100

    feat: add decrypt endpoint

---
+++ app/interactions/crypto/base64/decrypt.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class Crypto::Base64::Decrypt < ApplicationInteraction
+    hash :payload, strip: false, required: true
+
+    def execute
+        decrypted_payload
+    end
+
+    private
+
+    def decrypted_payload
+        @decrypted_payload ||= payload.transform_values do |value|
+            JSON.parse(Base64.decode64(value))
+        end
+    rescue JSON::ParserError, ArgumentError
+        errors.add(:base, "Invalid payload")
+    end
+end
